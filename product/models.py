c4162c9a95a38720f26d78869a22ec1f812df287
anika ahsan <anika.ahsan@g.bracu.ac.bd>
2023-02-08 18:24:37 -0500

    product app initialized

---
+++ product/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=250)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
+    quantity=models.IntegerField()
+    
